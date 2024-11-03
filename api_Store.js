a4b48789a98467e2c636bfb21902a4f0d745894a
Harsh Patel <harshp5@usf.edu>
2024-11-03 12:04:45 -0500

    Format product information dynamically, and Handle errors with .catch() and display error message.

--- api_Store.js
+++ api_Store.js
@@ -13,7 +13,22 @@ fetch(apiURL)
         // this is to call all the function to display products
         displayProducts(data);
     })
+    // this is task 4
     .catch(error => {
         // this is to handle any errors that occur during the fetch
         console.error("Failed to load products. Please try again later.", error);
     });
+// Task 3 Display Product Details Dynamically
+function displayProducts(products) {
+    const productContainer = document.getElementById("product-container");
+    products.forEach(product => {
+        const productElement = document.createElement('div');
+        productElement.innerHTML = `
+            <h2>${product.name}</h2>
+            <img src="${product.image}" alt="${product.name}" />
+            <p>Company: ${product.company}</p>
+            <p>Price: $${product.price}</p>
+        `;
+        productContainer.appendChild(productElement);
+    });
+}
